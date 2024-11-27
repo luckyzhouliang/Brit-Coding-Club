9bbd9f908fef0dd10831f35b129f0d11c7898e47
zhouliang <zhouliang18@outlook.com>
2024-11-27 21:53:00 +0800

    Using template, 使用模板和渲染

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-from flask import Flask
-
-from flask import url_for
-from markupsafe import escape
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name = 'Zhou Liang'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/')
-def hello():
-    return 'Hello'
-
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-
-
-@app.route('/test')
-def test_url_for():
-    return 'Test page'
+def index():
+    return render_template('index.html', movies=movies, name=name)
 
 
 if __name__ == '__main__':
