73cc2e031f2e94b3239f02921c69bad7eef905cd
zhouliang <zhouliang18@outlook.com>
2024-12-09 10:39:44 +0800

    部署版本

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -7,9 +7,9 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 prefix = 'sqlite:///'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
 
 db = SQLAlchemy(app)
 
